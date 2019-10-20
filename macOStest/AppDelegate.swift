d44c519de0fc924a76016572a786821d0bc0528a
Danny <dtoomey@umail.ucsb.edu>
2019-10-20 14:10:19 -0700

    Initial Commit

---
+++ macOStest/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  macOStest
+//
+//  Created by Danny Toomey on 10/20/19.
+//  Copyright © 2019 Danny Toomey. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
